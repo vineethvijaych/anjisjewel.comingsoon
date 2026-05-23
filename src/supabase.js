fcfbae5cdfd32024d7166140805a8736cf2d2e3d
vineethvijaych <vineethcpz6881.com>
2026-05-23 18:22:14 +0530

    use legacy JWT anon key

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://dykwmbvftulelqfnvvgt.supabase.co";
-const supabaseAnonKey = "sb_publishable_TfxAEWA5Ut17OEtu5gfxvw_vMZwX9aC";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR5a3dtYnZmdHVsZWxxZm52dmd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk1MzA0NTgsImV4cCI6MjA5NTEwNjQ1OH0.eUBdN3MrBqZwu4zXBnvALHm1u0KBfxp5hzZzokVeE0s";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
